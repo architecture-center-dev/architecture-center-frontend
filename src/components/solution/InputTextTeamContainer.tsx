aced16fedf4d2a664fc3fbd2aef74a3abdda176b
Robisson Oliveira <robisson@robissonoliveira.com.br>
2021-01-24 12:45:40 -0300

    Fix syntax

--- src/components/solution/InputTextTeamContainer.tsx
+++ src/components/solution/InputTextTeamContainer.tsx
@@ -1,42 +1,48 @@
 import { useMutation } from '@apollo/client';
 import React from 'react';
 import InputTextTag from "./InputTextTag";
-import {ADD_MEMBER_TO_SOLUTION} from "./operations/mutaions";
+import { ADD_MEMBER_TO_SOLUTION } from "./operations/mutaions";
 
 const ENTER_KEY = 13;
 
-const InputTextTagContainer = ({solution_id, setVisible}) => {
+export type InputTextTagContainerProps = {
+    solution_id: string,
+    setVisible: Function
+}
+
+const InputTextTagContainer = ({ solution_id, setVisible }: InputTextTagContainerProps) => {
 
     const [saveTemMeber] = useMutation(ADD_MEMBER_TO_SOLUTION);
 
-    const onKeyUp = ({keyCode, target})=> {
-        if(keyCode == ENTER_KEY){
+    const onKeyUp = ({ keyCode, target }: KeyboardEvent) => {
+        if (keyCode == ENTER_KEY) {
             saveTemMeber(
-                {variables:{ solution_id, team_member: target.value },
-                update: (cache, {data:{createTeamMemberSolution}}) => {
-                    cache.modify({
-                      fields: {
-                        solutionById: (existingSolution) => ({...existingSolution, team_members: createTeamMemberSolution.team_members})
-                      }
-                    })
-                  }
-            });
+                {
+                    variables: { solution_id, team_member: (target as HTMLInputElement).value },
+                    update: (cache, { data: { createTeamMemberSolution } }) => {
+                        cache.modify({
+                            fields: {
+                                solutionById: (existingSolution) => ({ ...existingSolution, team_members: createTeamMemberSolution.team_members })
+                            }
+                        })
+                    }
+                });
 
-            target.value = '';
+            (target as HTMLInputElement).value = '';
             setVisible(false);
         }
     }
-    
-    const onFocusOut = ()=> {
+
+    const onFocusOut = () => {
         setVisible(false);
     }
 
-    return(
-        <InputTextTag 
-            label="Type and press ENTER" 
+    return (
+        <InputTextTag
+            label="Type and press ENTER"
             onKeyUp={onKeyUp}
             onFocusOut={onFocusOut}
-            />
+        />
     )
 }
 
